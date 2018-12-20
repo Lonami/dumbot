439427c81cc05d20d5fe05f25fc66ae62b044be2
Lonami Exo <totufals@hotmail.com>
2018-12-20 19:29:52 +0100

    Assert that method names have upper case letters
    This prevents common errors

--- dumbot.py
+++ dumbot.py
@@ -158,6 +158,15 @@ class Bot:
             'dumbot{}'.format(token[:token.index(':')]))
 
     def __getattr__(self, method_name):
+        if not any(c.isupper() for c in method_name):
+            # All method calls are dynamic names. It's very easy to
+            # accidentally try to access a member (snake_case) that doesn't
+            # exist, but this method would swallow the error. All methods in
+            # the API have at least one upper-case letter in their name, so
+            # if the method name doesn't have one, the user probably accessed
+            # an attribute that doesn't exist yet. Let it raise.
+            return super().__getattribute__(method_name)
+
         async def request(**kwargs):
             fp = None
             file = kwargs.pop('file', None)
